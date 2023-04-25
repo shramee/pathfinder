0652470ee235b37ce58d25a43bda24660b04a1f1
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-04-26 05:51:02 +0800

    Fixing tests in versioning

--- crates/rpc/src/versioning.rs
+++ crates/rpc/src/versioning.rs
@@ -229,8 +229,7 @@ mod tests {
         use serde_json::json;
 
         let context = RpcContext::for_tests();
-        // unused websocket_txs: WebsocketSenders
-        let (_server_handle, _, address) = RpcServer::new("127.0.0.1:0".parse().unwrap(), context)
+        let (_server_handle, address) = RpcServer::new("127.0.0.1:0".parse().unwrap(), context)
             .run()
             .await
             .unwrap();
@@ -285,8 +284,7 @@ mod tests {
         use crate::{RpcContext, RpcServer};
 
         let context = RpcContext::for_tests();
-        // unused websocket_txs: WebsocketSenders
-        let (_server_handle, _, address) = RpcServer::new("127.0.0.1:0".parse().unwrap(), context)
+        let (_server_handle, address) = RpcServer::new("127.0.0.1:0".parse().unwrap(), context)
             .run()
             .await
             .unwrap();
